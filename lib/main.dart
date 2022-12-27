3e1a0348710c93378873de6bdeeac95eedaaaa94
shunsuke-tamura <wwwxx4869@gmail.com>
2022-12-27 23:06:25 +0900

    update: containerの演習

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,62 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            // デフォルト表示
-            const Text('Default'),
-            // 太さを指定
-            const Text('Bold', style: TextStyle(fontWeight: FontWeight.bold)),
-            // スタイルを指定
-            const Text('Italic', style: TextStyle(fontStyle: FontStyle.italic)),
-            // サイズを指定
-            const Text('fontSize = 36', style: TextStyle(fontSize: 36)),
-            // 色を指定
-            const Text('Red', style: TextStyle(color: Colors.red)),
-            Container(
-              width: double.infinity,
-              color: Colors.grey,
-              // 表示位置を指定
-              child: const Text('TextAlign.right', textAlign: TextAlign.right),
-            ),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                // 背景色
+                color: Colors.blue,
+                child: const Text('blue'),
+              ),
+              Container(
+                // 横幅
+                width: 200,
+                // 縦幅
+                height: 50,
+                color: Colors.blue,
+                child: const Text('200x50'),
+              ),
+              Container(
+                // 内側の余白
+                padding: const EdgeInsets.all(8),
+                color: Colors.blue,
+                child: const Text('padding'),
+              ),
+              Container(
+                color: Colors.blue,
+                child: Container(
+                  // 外側の余白
+                  margin: const EdgeInsets.all(8),
+                  color: Colors.green,
+                  child: const Text('margin'),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  // 枠線
+                  border: Border.all(color: Colors.blue, width: 2),
+                  // 角丸
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: const Text('border'),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  // 背景画像
+                  image: DecorationImage(
+                    // chromeで`--web-renderer html`をつけないと表示されない
+                    image: NetworkImage('https://placehold.jp/200x100.png'),
+                  ),
+                ),
+                width: 200,
+                height: 100,
+                child: const Text('image'),
+              ),
+            ],
+          ),
         ),
       ),
     );
