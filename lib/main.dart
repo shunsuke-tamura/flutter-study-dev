8bfe7eb693665079dc1ea4dab59325bf40924286
shunsuke-tamura <wwwxx4869@gmail.com>
2022-12-27 23:18:07 +0900

    update: columnとrowの演習

--- lib/main.dart
+++ lib/main.dart
@@ -11,62 +11,90 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                // 背景色
-                color: Colors.blue,
-                child: const Text('blue'),
+        body: Column(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.grey[200],
+              child: Row(
+                // 横に並べる
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('first')),
+                  Container(color: Colors.blue, child: const Text('second')),
+                  Container(color: Colors.green, child: const Text('third')),
+                ],
               ),
-              Container(
-                // 横幅
-                width: 200,
-                // 縦幅
-                height: 50,
-                color: Colors.blue,
-                child: const Text('200x50'),
+            ),
+            SizedBox(
+              // colorを指定しない時はSizeBoxでOKらしい
+              width: double.infinity,
+              height: 60,
+              child: Row(
+                // 中央寄せ
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('***')),
+                  Container(color: Colors.blue, child: const Text('中央寄せ')),
+                  Container(color: Colors.green, child: const Text('---')),
+                ],
               ),
-              Container(
-                // 内側の余白
-                padding: const EdgeInsets.all(8),
-                color: Colors.blue,
-                child: const Text('padding'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.grey[200],
+              child: Row(
+                // 右寄せ
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('***')),
+                  Container(color: Colors.blue, child: const Text('右寄せ')),
+                  Container(color: Colors.green, child: const Text('---')),
+                ],
               ),
-              Container(
-                color: Colors.blue,
-                child: Container(
-                  // 外側の余白
-                  margin: const EdgeInsets.all(8),
-                  color: Colors.green,
-                  child: const Text('margin'),
-                ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: Row(
+                // 均等配置
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('***')),
+                  Container(color: Colors.blue, child: const Text('均等配置')),
+                  Container(color: Colors.green, child: const Text('---')),
+                ],
               ),
-              Container(
-                decoration: BoxDecoration(
-                  // 枠線
-                  border: Border.all(color: Colors.blue, width: 2),
-                  // 角丸
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                padding: const EdgeInsets.all(8),
-                child: const Text('border'),
+            ),
+            Container(
+              width: double.infinity,
+              height: 60,
+              color: Colors.grey[200],
+              child: Row(
+                // 上寄せ
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('***')),
+                  Container(color: Colors.blue, child: const Text('上寄せ')),
+                  Container(color: Colors.green, child: const Text('---')),
+                ],
               ),
-              Container(
-                decoration: const BoxDecoration(
-                  // 背景画像
-                  image: DecorationImage(
-                    // chromeで`--web-renderer html`をつけないと表示されない
-                    image: NetworkImage('https://placehold.jp/200x100.png'),
-                  ),
-                ),
-                width: 200,
-                height: 100,
-                child: const Text('image'),
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: Row(
+                // 下寄せ
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(color: Colors.red, child: const Text('***')),
+                  Container(color: Colors.blue, child: const Text('下寄せ')),
+                  Container(color: Colors.green, child: const Text('---')),
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
